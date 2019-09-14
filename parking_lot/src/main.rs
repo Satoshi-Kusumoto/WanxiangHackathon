a182577706f850f6dab2f816f2f3ab04c76da799
yjhmelody <465402634@qq.com>
2019-09-14 15:15:48 +0800

    refactor template and update config

--- parking_lot/src/main.rs
+++ parking_lot/src/main.rs
@@ -4,22 +4,25 @@
 #![warn(unused_extern_crates)]
 
 mod chain_spec;
+#[macro_use]
 mod service;
 mod cli;
 
-pub use substrate_cli::{VersionInfo, IntoExit, error};
+pub use substrate_cli::{error, IntoExit, VersionInfo};
 
-fn run() -> cli::error::Result<()> {
-	let version = VersionInfo {
-		name: "Substrate Node",
-		commit: env!("VERGEN_SHA_SHORT"),
-		version: env!("CARGO_PKG_VERSION"),
-		executable_name: "parking-lot",
-		author: "yjhmelody",
-		description: "parking_lot",
-		support_url: "support.anonymous.an",
-	};
-	cli::run(::std::env::args(), cli::Exit, version)
+fn main() {
+    let version = VersionInfo {
+        name: "Substrate Node",
+        commit: env!("VERGEN_SHA_SHORT"),
+        version: env!("CARGO_PKG_VERSION"),
+        executable_name: "wx-node",
+        author: "yjhmelody",
+        description: "parking_lot",
+        support_url: "http://github.com/yjhmelody",
+    };
+
+    if let Err(e) = cli::run(::std::env::args(), cli::Exit, version) {
+        eprintln!("Fatal error: {}\n\n{:?}", e, e);
+        std::process::exit(1)
+    }
 }
-
-error_chain::quick_main!(run);
